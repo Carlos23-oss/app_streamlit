fc222e721ad9dfdfe9d2ccf822a7b025830e1bcd
Carlos Onrubia <c.onrubia11@gmail.com>
2025-05-25 23:12:54 +0200

    Update app_recommender.py

--- app_recommender.py
+++ app_recommender.py
@@ -10,9 +10,9 @@ import gdown
 os.makedirs("recommender_artifacts", exist_ok=True)
 
 # Descargar archivos grandes desde Google Drive (reemplaza con tus IDs reales)
-gdown.download("https://drive.google.com/file/d/1-1aiZmE-_N40IS4vEHfXX5fSdZhXuCBb/view?usp=drive_link", "recommender_artifacts/df_rec.parquet", quiet=False)
-gdown.download("https://drive.google.com/file/d/1-23W8XpXbyjQo99BEjGpnpWFqaYqwKC5/view?usp=drive_link", "recommender_artifacts/embeddings.npy", quiet=False)
-gdown.download("https://drive.google.com/file/d/1-A3LtK-PkW7FC624c_NwEytRGRvOgq_n/view?usp=drive_link", "recommender_artifacts/faiss.index", quiet=False)
+gdown.download("https://drive.google.com/file/d/1-1aiZmE-_N40IS4vEHfXX5fSdZhXuCBb/view?usp=sharing", "recommender_artifacts/df_rec.parquet", quiet=False)
+gdown.download("https://drive.google.com/file/d/1-23W8XpXbyjQo99BEjGpnpWFqaYqwKC5/view?usp=sharing", "recommender_artifacts/embeddings.npy", quiet=False)
+gdown.download("https://drive.google.com/file/d/1-A3LtK-PkW7FC624c_NwEytRGRvOgq_n/view?usp=sharing", "recommender_artifacts/faiss.index", quiet=False)
 
 # Funciones y app (igual que antes)
 # Puedes pegar aquí el resto de tu código, desde load_artifacts() hasta el final
